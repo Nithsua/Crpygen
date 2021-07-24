6e3116227ecc8c6f423577bf0d906990bad60de2
Nivas Muthu M G <nivasmuthumg@icloud.com>
2021-07-24 09:12:31 +0530

    Initial Commit

---
+++ Crypgen/CrypgenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypgenApp.swift
+//  Crypgen
+//
+//  Created by Nivas Muthu M G on 2021-07-24.
+//
+
+import SwiftUI
+
+@main
+struct CrypgenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
